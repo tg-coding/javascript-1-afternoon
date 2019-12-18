3ce0929ce970806254aec174c4c56eb92d80015a
Tia Gollaher <tiaag2013@gmail.com>
2019-12-18 16:28:21 -0700

    Finished all problems

--- practice.js
+++ practice.js
@@ -2,7 +2,7 @@
 
 //Create a variable called myName that is a string data type
 
-var myName = 'Tia';
+const myName = 'Tia';
 
 //////////////////PROBLEM 2////////////////////
 
@@ -125,7 +125,17 @@ mathSum = add(3,4)
 // If the passed in color equals 'black', return 'so trendy'
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
 
-function faveColorFinder(color){}
+function faveColorFinder(color){
+  if(color === 'red') {
+    return 'red is a great color'
+  } else if(color === 'green') {
+    return 'green is a solid favorite color'
+  } else if(color === 'black') {
+  return 'so trendy'
+  } else {
+    return `you need to evaluate your favorite color choice`
+  }
+}
 
 //////////////////PROBLEM 13////////////////////
 
@@ -149,28 +159,29 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 14////////////////////
 
 //Create a variable called age with your age assigned to you
 
-// Code Here
+var age = 28
+
 
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
-// Code Here
+age = 29
 
 // Good news! We can live forever. Set your age to 999
 
-// Code Here
+age = 999
